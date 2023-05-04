8897e6c03d0f0981fe5d12f4f82de0f38cdf3417
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-05-04 11:35:05 +0200

    lavoro corretto

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
+//dichiariamo variabili
+let somma = 0;
+
 //Chiediamo all'utente per 5 volte di inserire un numero. 
 
-let numberOne = parseInt (prompt("Inserisci il primo numero"));
+for (let i = 1; i <= 5; i++) {
+    const numero = parseInt (prompt("Inserisci un numero"));
+    console.log(numero);
+    somma = somma + numero;
+}
+
+console.log(somma);
+
+
+/*let numberOne = parseInt (prompt("Inserisci il primo numero"));
 let numberTwo = parseInt (prompt("Inserisci il secondo numero"));
 let numberThree = parseInt (prompt("Inserisci il terzo numero"));
 let numberFour = parseInt (prompt("Inserisci il quarto numero"));
@@ -15,4 +27,4 @@ console.log("5: " + numberFive);
  //Il programma stampa la somma di tutti i numeri inseriti.
 
  let total = numberOne + numberTwo + numberThree + numberFour + numberFive;
- console.log("total: " + total);
+ console.log("total: " + total); */
